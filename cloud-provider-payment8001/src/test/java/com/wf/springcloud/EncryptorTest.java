96fef04bd4025b1c30c684295e0c56f7235dfc43
wf <285682283@qq.com>
2020-04-25 20:42:16 +0800

    springcloud2020-学习-- 关键信息加密

---
+++ cloud-provider-payment8001/src/test/java/com/wf/springcloud/EncryptorTest.java
@@ -0,0 +1,44 @@
+package com.wf.springcloud;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * @author wf
+ * @create 2020-04-25 18:31
+ * @desc
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EncryptorTest {
+
+    @Resource
+    private ApplicationContext applicationContext;
+    @Resource(name = "wfEncryptorBean")
+    private StringEncryptor stringEncryptor;
+
+    @Test
+    public void run() throws Exception {
+        Environment environment = applicationContext.getBean(Environment.class);
+
+        // 首先获取配置文件里的原始明文信息
+        String mysqlOriginPswd = environment.getProperty("spring.datasource.password");
+        String mysqlEncryptedPswd = encrypt(mysqlOriginPswd);
+        System.out.println("MySQL原始明文密码加密后的结果为：" + mysqlEncryptedPswd);
+    }
+
+    private String encrypt(String originPassord) {
+        return stringEncryptor.encrypt(originPassord);
+    }
+
+    private String decrypt(String encryptedPassword) {
+        return stringEncryptor.decrypt(encryptedPassword);
+    }
+}
